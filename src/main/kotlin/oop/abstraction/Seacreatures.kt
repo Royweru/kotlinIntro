28c3891e7e0e2eea19102eade56d61bab5750629
30Dennis <otienodennis69@gmail.com>
2023-08-17 10:30:19 +0300

    OOP in kotlin

--- src/main/kotlin/oop/abstraction/Seacreatures.kt
+++ src/main/kotlin/oop/abstraction/Seacreatures.kt
@@ -4,10 +4,29 @@ abstract  class Seacreatures(val name: String) {
     abstract fun makeSound()
 
     fun describe(){
-        println("$name makes sound")
+        println("makes sound $name ")
     }
 }
 
 class Whale(name: String):Seacreatures(name){
+    override fun makeSound() {
+        println("A whale makes the sound $name")
+    }
+}
 
+class Shark(name: String):Seacreatures(name){
+    override fun makeSound() {
+        println("A shark makes the sound $name")
+    }
+}
+
+fun main(args: Array<String>) {
+    val creature1:Seacreatures = Whale("blup blup")
+    val creature2:Seacreatures = Shark("whi whi")
+
+    creature1.describe()
+    creature1.makeSound()
+
+    creature2.describe()
+    creature2.makeSound()
 }
